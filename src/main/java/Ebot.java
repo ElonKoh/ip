ad41468943cd56041507085f4f8cc072f1ef4374
Elon Koh <e0552367@u.nus.edu>
2025-02-02 19:39:22 +0800

    Added echo-user functionality and some chat formatting

--- src/main/java/Ebot.java
+++ src/main/java/Ebot.java
@@ -1,10 +1,33 @@
+import java.util.Scanner;
+
 public class Ebot {
+
+
     public static void main(String[] args) {
-        String lineSeparator = ("__________________________________");
-        String welcomeMessage = ("Hello! I'm Ebot \n" +
-                "What can i do for you?");
-        String existMessage = ("Bye. Hope to see you again soon!");
-        System.out.println(lineSeparator + "\n" + welcomeMessage);
-        System.out.println(lineSeparator + "\n" + existMessage);
+        String lineDivider = ("___________________________________________");
+        String bigIndent = "\t\t";
+        String smallIndent = "\t";
+        String welcomeMessage = (smallIndent + "Hello! I'm Ebot" +
+                System.lineSeparator() +
+                smallIndent + "What can i do for you?");
+        String exitMessage = (smallIndent + "Bye. Hope to see you again soon!");
+        System.out.println(lineDivider + System.lineSeparator() + welcomeMessage);
+        System.out.println(lineDivider);
+
+        // Program start
+        String ebotSalutation = "\tEbot says: ";
+        String userInput;
+        Scanner in = new Scanner(System.in);
+        userInput = "";
+        while (!userInput.equalsIgnoreCase("bye")) {
+            userInput = in.nextLine();
+            if (!userInput.equalsIgnoreCase("bye")) {
+                System.out.println(ebotSalutation + System.lineSeparator() + bigIndent + userInput);
+                System.out.println(lineDivider);
+            } else {
+                System.out.println(exitMessage);
+                System.out.println(lineDivider);
+            }
+        }
     }
 }
