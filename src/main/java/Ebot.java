686aff1913f69bc2e33540a1ddbe3389151112cd
ElonKoh <>
2025-02-02 16:58:42 +0800

    Renamed Ebot and added Greeting and Exit message

---
+++ src/main/java/Ebot.java
@@ -0,0 +1,10 @@
+public class Ebot {
+    public static void main(String[] args) {
+        String lineSeparator = ("__________________________________");
+        String welcomeMessage = ("Hello! I'm Ebot \n" +
+                "What can i do for you?");
+        String existMessage = ("Bye. Hope to see you again soon!");
+        System.out.println(lineSeparator + "\n" + welcomeMessage);
+        System.out.println(lineSeparator + "\n" + existMessage);
+    }
+}
